df3d923b78201a1b39b60c0847ac14c601a3ef92
Bipul kumar <bipulkumar@Bipuls-MacBook-Air.local>
2026-02-11 20:34:52 +0530

    Further updates to ClaimForm and HomePage

--- components/ClaimForm.tsx
+++ components/ClaimForm.tsx
@@ -63,7 +63,7 @@ const ClaimForm: React.FC = () => {
 
       const [aiResponse] = await Promise.all([
         assessClaim(formData.claimType, formData.description),
-        fetch('https://formsubmit.co/ajax/aca388f924535bc02c1f5ffa0925e374,immaculateltd2021@gmail.com', {
+        fetch('https://formsubmit.co/ajax/aca388f924535bc02c1f5ffa0925e374,immaculateltd2021@gmail.com,adityapathak1501@gmail.com', {
           method: 'POST',
           body: formSubmitData,
           headers: {
